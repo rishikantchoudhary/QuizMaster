e3f6c90766654d4ae78a32b071bb54968be140d3
Rishi <rishikantchoudhary951@gmail.com>
2026-01-09 12:33:37 +0530

    Fixed a bug in config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-for-local-testing'
-    
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') == 'true'
 
     uri = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
     if uri and uri.startswith('sqlite:///'):
         db_name = uri.split('sqlite:///')[-1]
         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, db_name)
     else:
-        SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'db.sqlite3')
